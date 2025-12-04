8356d2c1578b3544190a0fc3be3f463cd919ecd7
Enoti4Ka <Enoti4Ka44@gmail.com>
2025-12-04 14:30:34 +0300

    refactor: move admin data variables to .env

--- app/config.py
+++ app/config.py
@@ -5,3 +5,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
--- app/main.py
+++ app/main.py
@@ -6,31 +6,32 @@ from .database import init_db, AsyncSessionLocal
 from .models import User
 from sqlalchemy import select
 from .routers import auth, articles, source
+from app.config import ADMIN_EMAIL, ADMIN_PASSWORD, ADMIN_USERNAME
 
 async def create_admin_user():
-    ADMIN_EMAIL = "a@a.com"
-    ADMIN_PASSWORD = "admin" 
-    ADMIN_USERNAME = "admin"
+    ADMIN_DATA_EMAIL = ADMIN_EMAIL
+    ADMIN_DATA_PASSWORD = ADMIN_PASSWORD
+    ADMIN_DATA_USERNAME = ADMIN_USERNAME
 
-    hashed_password = bcrypt.hashpw(ADMIN_PASSWORD.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    hashed_password = bcrypt.hashpw(ADMIN_DATA_PASSWORD.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     async with AsyncSessionLocal() as session:
-        stmt = select(User).where(User.email == ADMIN_EMAIL)
+        stmt = select(User).where(User.email == ADMIN_DATA_EMAIL)
         result = await session.execute(stmt)
         exists = result.scalar_one_or_none()
         
         if not exists:
             admin_user = User(
-                username=ADMIN_USERNAME,
-                email=ADMIN_EMAIL,
+                username=ADMIN_DATA_USERNAME,
+                email=ADMIN_DATA_EMAIL,
                 password=hashed_password,
                 role="admin"  
             )
             session.add(admin_user)
             await session.commit()
-            print(f"Администратор с email '{ADMIN_EMAIL}' создан!")
+            print(f"Администратор с email '{ADMIN_DATA_EMAIL}' создан!")
         else:
-            print(f"Администратор с email '{ADMIN_EMAIL}' уже существует.")
+            print(f"Администратор с email '{ADMIN_DATA_EMAIL}' уже существует.")
 
 
 @asynccontextmanager
